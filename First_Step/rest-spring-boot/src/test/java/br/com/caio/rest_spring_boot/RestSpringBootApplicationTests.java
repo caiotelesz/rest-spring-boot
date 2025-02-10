27dda11a3ef85d58881ad5bc141db3ea06901f4e
caiotelesz <caioteles1267@gmail.com>
2025-02-10 15:04:57 -0300

    creating spring

---
+++ First_Step/rest-spring-boot/src/test/java/br/com/caio/rest_spring_boot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
