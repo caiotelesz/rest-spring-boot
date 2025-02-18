af35b1cc5ca6db5d940e7575ced103aca303870c
caiotelesz <caioteles1267@gmail.com>
2025-02-18 11:20:15 -0300

    creating findAll

--- Spring21.2/rest-spring-boot/src/main/java/br/com/caio/PersonServices.java
+++ Spring21.2/rest-spring-boot/src/main/java/br/com/caio/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.caio;
 import br.com.caio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,17 @@ public class PersonServices {
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
     public Person findById(String id) {
         logger.info("Finding one Person!");
 
@@ -21,6 +34,17 @@ public class PersonServices {
         person.setFirstName("Caio");
         person.setLastName("Teles");
         person.setAddress("São Paulo");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("FirstName " + i);
+        person.setAddress("Some Address in Brazil ");
         person.setGender("Male");
 
         return person;
--- Spring21.2/rest-spring-boot/src/main/java/br/com/caio/controllers/PersonController.java
+++ Spring21.2/rest-spring-boot/src/main/java/br/com/caio/controllers/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -16,7 +18,16 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @RequestMapping(value = "/{id}",
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(
+            value = "/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
