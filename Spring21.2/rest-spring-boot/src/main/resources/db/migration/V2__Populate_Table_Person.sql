65c88be55203b9efeb95ef123a34be8f03241403
caiotelesz <caioteles1267@gmail.com>
2025-02-25 13:00:57 -0300

    Learning Migrations with Flyway

---
+++ Spring21.2/rest-spring-boot/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'São Paulo, Brasil', 'Caio', 'Male', 'Teles'),
+    (2, 'Barcelona, Espanha', 'Juan', 'Male', 'Souza'),
+    (4, 'Milan, Italia', 'Francisco', 'Male', 'Ferrari'),
+    (5, 'Turim, Italia', 'Chiesa', 'Male', 'Francesco'),
+    (7, 'Bosnia Herzegovina', 'Ronald', 'Male', 'Chezko'),
+    (8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+    (14, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
