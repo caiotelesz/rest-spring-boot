48f950f7aa6d964be75ffcdc68391b6a4d620419
caiotelesz <caioteles1267@gmail.com>
2025-03-05 20:25:11 -0300

    Book: creating controller

---
+++ Spring21.3/rest-spring-boot/src/main/java/br/com/caio/controllers/book/BookController.java
@@ -0,0 +1,55 @@
+package br.com.caio.controllers.book;
+
+import br.com.caio.data.dto.book.BookDTO;
+import br.com.caio.services.book.BookServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/book")
+public class BookController {
+
+    @Autowired
+    private BookServices bookServices;
+
+    @GetMapping(produces = {
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public List<BookDTO> findAll() {
+        return bookServices.findAll();
+    }
+
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public BookDTO findById(@PathVariable Long id) {
+        return bookServices.findById(id);
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public BookDTO save(@RequestBody BookDTO book) {
+        return bookServices.createBook(book);
+    }
+
+    @PutMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public BookDTO update(@RequestBody BookDTO book) {
+        return bookServices.updateBook(book);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        bookServices.deleteBookPerId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
