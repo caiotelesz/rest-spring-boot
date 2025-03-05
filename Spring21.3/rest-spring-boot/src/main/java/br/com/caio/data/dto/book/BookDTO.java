da5347539be27a2be5d09e9cbaf649715d25a4c9
caiotelesz <caioteles1267@gmail.com>
2025-03-05 19:53:31 -0300

    Book: creating dto

---
+++ Spring21.3/rest-spring-boot/src/main/java/br/com/caio/data/dto/book/BookDTO.java
@@ -0,0 +1,73 @@
+package br.com.caio.data.dto.book;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class BookDTO extends RepresentationModel<BookDTO> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String author;
+    private LocalDateTime launchDate;
+    private Double price;
+    private String title;
+
+    public BookDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(LocalDateTime launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookDTO bookDTO = (BookDTO) o;
+        return Objects.equals(id, bookDTO.id) && Objects.equals(author, bookDTO.author) && Objects.equals(launchDate, bookDTO.launchDate) && Objects.equals(price, bookDTO.price) && Objects.equals(title, bookDTO.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, launchDate, price, title);
+    }
+}
