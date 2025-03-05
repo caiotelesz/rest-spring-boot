16e0b6f4d35c3d7dad2ae575560c1e58ffce87d7
caiotelesz <caioteles1267@gmail.com>
2025-03-05 20:41:27 -0300

    Book: implementing swagger & open api

--- Spring21.3/rest-spring-boot/src/main/java/br/com/caio/controllers/book/BookController.java
+++ Spring21.3/rest-spring-boot/src/main/java/br/com/caio/controllers/book/BookController.java
@@ -1,5 +1,6 @@
 package br.com.caio.controllers.book;
 
+import br.com.caio.controllers.book.docs.BookControllerDocs;
 import br.com.caio.data.dto.book.BookDTO;
 import br.com.caio.services.book.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/book/v1")
-public class BookController {
+public class BookController implements BookControllerDocs {
 
     @Autowired
     private BookServices bookServices;
@@ -21,6 +22,7 @@ public class BookController {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_YAML_VALUE
     })
+    @Override
     public List<BookDTO> findAll() {
         return bookServices.findAll();
     }
@@ -28,48 +30,52 @@ public class BookController {
     @GetMapping(
             value = "/{id}",
             produces = {
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-                MediaType.APPLICATION_YAML_VALUE
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
             }
     )
+    @Override
     public BookDTO findById(@PathVariable Long id) {
         return bookServices.findById(id);
     }
 
     @PostMapping(
             consumes = {
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-                MediaType.APPLICATION_YAML_VALUE
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
             },
             produces = {
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-                MediaType.APPLICATION_YAML_VALUE
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
             }
     )
+    @Override
     public BookDTO save(@RequestBody BookDTO book) {
         return bookServices.createBook(book);
     }
 
     @PutMapping(
             consumes = {
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-                MediaType.APPLICATION_YAML_VALUE
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
             },
             produces = {
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-                MediaType.APPLICATION_YAML_VALUE
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
             }
     )
+    @Override
     public BookDTO update(@RequestBody BookDTO book) {
         return bookServices.updateBook(book);
     }
 
     @DeleteMapping(value = "/{id}")
+    @Override
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         bookServices.deleteBookPerId(id);
         return ResponseEntity.noContent().build();
